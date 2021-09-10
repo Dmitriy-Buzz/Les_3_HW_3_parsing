47a71152d160d5069cc8a1261e7f8e65ce929f8d
Кукушкин Дмитрий Владимирович <Kukushkin_DV@sz.cius-ees.ru>
2021-09-10 14:18:46 +0300

    Les_4_HW_3 v1

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+tags = ['ДТП', 'Спутник']
+
+URL = 'https://www.fontanka.ru'
+
+response = requests.get('https://www.fontanka.ru/')
+
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, features='html.parser')
+
+all_page =soup.find_all(class_='DZrl')
+
+for n in all_page:
+    news = n.find_all(class_='FVdn')
+    news = [new.text.strip() for new in news]
+    href = n.find(class_='FVdn').attrs.get('href')
+    times = n.find(class_='FVwf')
+    times = [time.text.strip() for time in times]
+    for i in news:
+        for t in tags:
+            if t in i:
+                print(f'Время публикации {times[0]}, название новости "{i}" ссылка на новость:{URL+href}')
