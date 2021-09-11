e2986789619a6367b67e49272ea02074a01eb245
Dmitriy Kukushkin <mrdikukushkin@yandex.ru>
2021-09-11 18:44:36 +0300

    Les_3_HW закомичена строка с выводов времени публикации статьи

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ for n in all_page:
     news = n.find_all(class_='FVdn')
     news = [new.text.strip() for new in news]
     href = n.find(class_='FVdn').attrs.get('href')
-    times = n.find(class_='FVwf')
-    times = [time.text.strip() for time in times]
+    # times = n.find(class_='FVwf')
+    # times = [time.text.strip() for time in times]
     for i in news:
         for t in tags:
             if t in i:
-                print(f'Время публикации {times[0]}, название новости "{i}" ссылка на новость:{URL+href}')
+                print(f'Название новости "{i}" ссылка на новость:{URL+href}')
+
+
+# Время публикации {times[0]}
